757428ecb93bdbd46c9754bdf3549977e005f0a0
Thiago Augusto <taugustoas@gmail.com>
2020-04-25 13:43:49 -0300

    add colors

--- script.js
+++ script.js
@@ -5,14 +5,27 @@ function addBoll() {
   var p1 = Math.floor(Math.random() * 500);
   var p2 = Math.floor(Math.random() * 400);
 
-  boll.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
+  boll.setAttribute("style", "left:"+p1+"px;top:"+p2+"px; background-color:"+gera_cor()+";");
   boll.setAttribute("onclick", "pop(this)");
 
   document.body.appendChild(boll);
-}
+  }
 function pop(element) {
   document.body.removeChild(element);
 }
 function start() {
     setInterval(addBoll, 1000);
+}
+
+// gera uma cor aleatória em hexadecimal
+function gera_cor(){
+    var hexadecimais = '0123456789ABCDEF';
+    var cor = '#';
+
+    // Pega um número aleatório no array acima
+    for (var i = 0; i < 6; i++ ) {
+    //E concatena à variável cor
+        cor += hexadecimais[Math.floor(Math.random() * 16)];
+    }
+    return cor;
 }
